69873c101502200e1ab63e161501fe128aa1a78c
Takeuchi <masao@MacBook-Ari-Masao.local>
2024-05-25 12:23:46 +0900

    settingsの修正

--- config/settings.py
+++ config/settings.py
@@ -110,7 +110,7 @@ else:
 
     db_from_env = dj_database_url.config()
     DATABASES = {
-        'default': dj_database_url.config()
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
 
 
