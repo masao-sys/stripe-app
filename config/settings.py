01ba761b45d7aa00c916b0b211d8565724fdf35d
Takeuchi <masao@MacBook-Ari-Masao.local>
2024-05-25 09:28:23 +0900

    settingsの修正

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['*']
 
