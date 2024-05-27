d7618b7b4f62fc197bb6296a88df805cda853623
Takeuchi <masao@MacBook-Ari-Masao.local>
2024-05-28 08:45:39 +0900

    ALLOWED_HOSTSの再設定

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'damp-waters-36767-cf3938692830.herokuapp.com']
 
 
 # Application definition
