e2fa9ae801c9cc0592b06c46c79a6df02ade95be
Takeuchi <masao@MacBook-Ari-Masao.local>
2024-05-28 17:43:29 +0900

    STATIC_URLの変更

--- config/settings.py
+++ config/settings.py
@@ -145,7 +145,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
