07ca865cfa21bb705c7b7ae5baf12a387d7997df
Takeuchi <masao@MacBook-Ari-Masao.local>
2024-09-26 20:05:00 +0900

    envファイルを再変更

--- config/settings.py
+++ config/settings.py
@@ -256,7 +256,7 @@ else:
     # cloudinary
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
     CLOUDINARY_STORAGE = {
-        'CLOUD_NAME': 'hs4jhkidu',
+        'CLOUD_NAME': env('CLOUDINARY_NAME'),
         'API_KEY': env('CLOUDINARY_API_KEY'),
         'API_SECRET': env('CLOUDINARY_API_SECRET')
     }
